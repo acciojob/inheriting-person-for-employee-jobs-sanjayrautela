bd77c01ffde148e71a96445e70885b1aa5a7ef7d
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-02-13 11:13:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function(){
-	return `Hello, my name is ${this.name}, I am ${this.age} years old.`;
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 }
 
 
@@ -17,7 +17,7 @@ Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function() {
-  return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`;
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
 
 // Do not change code below this line
